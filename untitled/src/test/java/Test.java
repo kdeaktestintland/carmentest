c45c20d430a769dc4e613718025ad78975c0032a
Kristof Deak <kristof.deak@ps.intland.com>
2020-10-30 16:09:44 +0100

    testcommit

---
+++ untitled/src/test/java/Test.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright by Intland Software
+ * <p>
+ * All rights reserved.
+ * <p>
+ * This software is the confidential and proprietary information
+ * of Intland Software. ("Confidential Information"). You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with Intland.
+ */
+public class Test {
+
+
+    private void test() {
+        System.out.println("Test Method");
+    }
+}
